0abf45d62bc39ec0c14d184dbeb8cb9c25b0a5ad
Raphael Tang <raphpb1912@gmail.com>
2022-05-14 20:22:27 +0800

    Commit cleanup (version 0.2.5)

---
+++ scripts/update_appcast.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Check for uncommitted changes, because git checkout is needed
+[[ "$(git diff-index HEAD)" ]] && {
+  echo "There are uncommitted changes. Please commit or stash them before continuing!"
+  exit 1
+}
+
+# Check if current branch is main
+[ "$(git rev-parse --abbrev-ref HEAD)" == "main" ] || {
+  git checkout main
+}
+
+./scripts/generate_appcast || exit 1
+
+git checkout gh-pages || exit 1
+git add appcast.xml
+git commit -m "Update appcast.xml"
+git push
+git checkout main
