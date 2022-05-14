f6a02976c7cd29f775872b3a97a9d3686ec9fee4
Raphael Tang <raphpb1912@gmail.com>
2022-05-14 18:18:44 +0800

    init gh-pages

--- scripts/build-and-run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-xcodebuild -project M1necraft.xcodeproj -scheme M1necraft build
-
-x=$( xcodebuild -showBuildSettings -project M1necraft.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
-
-DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/M1necraft.app/Contents/MacOS/M1necraft
